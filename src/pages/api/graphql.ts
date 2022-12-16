96c1c3b60447efb4773476c7a31192429363de83
Josh Calder <josh@opensaas.com.au>
2022-12-16 20:48:20 +1100

    move to apollo server

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,24 +1,11 @@
-import { createYoga } from 'graphql-yoga'
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { keystoneContext } from '../../keystone/context'
+import { ApolloServer } from '@apollo/server'
+import { startServerAndCreateNextHandler } from '@as-integrations/next'
 
-/*
-  An example of how to setup your own yoga graphql server
-  using the generated Keystone GraphQL schema.
-*/
-export const config = {
-	api: {
-		// Disable body parsing (required for file uploads)
-		bodyParser: false,
-	},
-}
+const apolloServer = new ApolloServer({
+  schema: keystoneContext.graphql.schema,
+})
 
-// Use Keystone API to create GraphQL handler
-export default createYoga<{
-	req: NextApiRequest
-	res: NextApiResponse
-}>({
-	graphqlEndpoint: '/api/graphql',
-	schema: keystoneContext.graphql.schema,
-	context: ({ req, res }) => keystoneContext.withRequest(req, res),
+export default startServerAndCreateNextHandler(apolloServer, {
+  context: async (req, res) => keystoneContext.withRequest(req, res),
 })
