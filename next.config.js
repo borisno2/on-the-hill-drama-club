25033e37d41f95febcd8eb5c36179525953632fd
Josh Calder <josh@opensaas.com.au>
2022-12-16 21:58:14 +1100

    add ts-gql

--- next.config.js
+++ next.config.js
@@ -2,47 +2,50 @@
 const withPreconstruct = require('@preconstruct/next')
 const KEYSTONE_URL = process.env.KEYSTONE_URL || 'http://localhost:4000'
 const Path = require('path')
+const { withTsGql } = require('@ts-gql/next')
 
 /** @type {import('next').NextConfig} */
-module.exports = withPreconstruct({
-	webpack(config) {
-		config.resolve.alias = {
-			...config.resolve.alias,
-			react: Path.dirname(require.resolve('react/package.json')),
-			'react-dom': Path.dirname(require.resolve('react-dom/package.json')),
-			'@keystone-6/core': Path.dirname(
-				require.resolve('@keystone-6/core/package.json')
-			),
-		}
-		config.externals = [...config.externals, '.prisma/client']
-		return config
-	},
-	experimental: {
-	  scrollRestoration: true,
-	},
-	images: {
-	  dangerouslyAllowSVG: true,
-	  remotePatterns: [
-		{
-		  protocol: 'https',
-		  hostname: 'tailwindui.com',
-		  pathname: '/img/**/*',
-		  port: ''
-		}
-	  ]
-	},
-	async rewrites() {
-		return {
-			beforeFiles: [
-				{
-					source: '/admin',
-					destination: `${KEYSTONE_URL}/admin`,
-				},
-				{
-					source: '/admin/:admin*',
-					destination: `${KEYSTONE_URL}/admin/:admin*`,
-				},
-			],
-		}
-	},
-})
+module.exports = withTsGql(
+  withPreconstruct({
+    webpack(config) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        react: Path.dirname(require.resolve('react/package.json')),
+        'react-dom': Path.dirname(require.resolve('react-dom/package.json')),
+        '@keystone-6/core': Path.dirname(
+          require.resolve('@keystone-6/core/package.json')
+        ),
+      }
+      config.externals = [...config.externals, '.prisma/client']
+      return config
+    },
+    experimental: {
+      scrollRestoration: true,
+    },
+    images: {
+      dangerouslyAllowSVG: true,
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'tailwindui.com',
+          pathname: '/img/**/*',
+          port: '',
+        },
+      ],
+    },
+    async rewrites() {
+      return {
+        beforeFiles: [
+          {
+            source: '/admin',
+            destination: `${KEYSTONE_URL}/admin`,
+          },
+          {
+            source: '/admin/:admin*',
+            destination: `${KEYSTONE_URL}/admin/:admin*`,
+          },
+        ],
+      }
+    },
+  })
+)
