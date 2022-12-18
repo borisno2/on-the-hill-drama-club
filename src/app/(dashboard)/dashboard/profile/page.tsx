eb564bea9e494fde6518c19c2f4396e4be624390
Josh Calder <josh@opensaas.com.au>
2022-12-18 11:10:57 +1100

    fix page types

--- src/app/(dashboard)/dashboard/profile/page.tsx
+++ src/app/(dashboard)/dashboard/profile/page.tsx
@@ -1,10 +1,10 @@
 import DashboardLayout from "../../DashboardLayout"
 
 
-export default function Students({ ...pageProps }) {
+export default function Students() {
 
     return (
-        <DashboardLayout PageName="Profile" {...pageProps}><div className="py-4">
+        <DashboardLayout PageName="Profile"><div className="py-4">
             <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
         </div>
         </DashboardLayout>
--- src/app/(dashboard)/dashboard/students/page.tsx
+++ src/app/(dashboard)/dashboard/students/page.tsx
@@ -1,10 +1,10 @@
 import DashboardLayout from "../../DashboardLayout"
 
 
-export default function Students({ ...pageProps }) {
+export default function Students() {
 
     return (
-        <DashboardLayout PageName="Students" {...pageProps}><div className="py-4">
+        <DashboardLayout PageName="Students"><div className="py-4">
             <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
         </div>
         </DashboardLayout>
