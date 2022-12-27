209527fcab0a6222c50795b3f44cc8023d83f9e7
Josh Calder <josh@opensaas.com.au>
2022-12-27 15:41:03 +1100

    build out register

--- src/app/(auth)/auth/register/head.tsx
+++ src/app/(auth)/auth/register/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Sign In - On the Hill Performing Arts</title>
+      <title>Register - On the Hill Performing Arts</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,7 +1,129 @@
 export default function RegisterForm({ callbackUrl, csrfToken }: { callbackUrl: string, csrfToken?: string }) {
     return (
-        <>
-            <title>Register - On the Hill Performing Arts</title>
-        </>
+        <form className='space-y-6' action="#" method="POST">
+            <div className="shadow sm:overflow-hidden sm:rounded-md">
+                <div className="space-y-6 bg-white py-6 px-4 sm:p-6">
+                    <div>
+                        <h3 className="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
+                        <p className="mt-1 text-sm text-gray-500">Use a permanent address where you can recieve mail.</p>
+                    </div>
+
+                    <div className="grid grid-cols-6 gap-6">
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                                First name
+                            </label>
+                            <input
+                                type="text"
+                                name="first-name"
+                                id="first-name"
+                                autoComplete="given-name"
+                                className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            />
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                                Last name
+                            </label>
+                            <input
+                                type="text"
+                                name="last-name"
+                                id="last-name"
+                                autoComplete="family-name"
+                                className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            />
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-4">
+                            <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
+                                Email address
+                            </label>
+                            <input
+                                type="text"
+                                name="email-address"
+                                id="email-address"
+                                autoComplete="email"
+                                className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            />
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                autoComplete="new-password"
+                                className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            />
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="password-confirm" className="block text-sm font-medium text-gray-700">
+                                Confirm Password
+                            </label>
+                            <input
+                                type="password"
+                                name="password-confirm"
+                                id="password-confirm"
+                                autoComplete="new-password"
+                                className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            />
+                        </div>
+
+
+                        <div className="col-span-6">
+                            <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
+                                Street address
+                            </label>
+                            <input
+                                type="text"
+                                name="street-address"
+                                id="street-address"
+                                autoComplete="street-address"
+                                className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            />
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                            <label htmlFor="city" className="block text-sm font-medium text-gray-700">
+                                City
+                            </label>
+                            <input
+                                type="text"
+                                name="city"
+                                id="city"
+                                autoComplete="address-level2"
+                                className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            />
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                            <label htmlFor="postalcode" className="block text-sm font-medium text-gray-700">
+                                Postcode
+                            </label>
+                            <input
+                                type="text"
+                                name="postalcode"
+                                id="postalcode"
+                                autoComplete="postal-code"
+                                className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button
+                        type="submit"
+                        className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                        Save
+                    </button>
+                </div>
+            </div>
+        </form>
     )
 }
