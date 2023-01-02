d8620aeab6bdf99bc6f0e9f680087b4eef5f5d90
Josh Calder <josh@opensaas.com.au>
2023-01-02 12:08:50 +1100

    Fix dashboard footer

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,6 +3,8 @@ import AuthContext from 'app/AuthContext'
 import 'styles/tailwind.css'
 import 'focus-visible'
 import { Footer } from 'components/Footer';
+import { Container } from 'components/Container';
+import Link from 'next/link';
 
 type AppProps = {
     children: React.ReactNode;
@@ -16,7 +18,21 @@ export default function RootLayout({ children }: AppProps) {
             <body className="h-full">
                 <AuthContext>
                     {children}
-                    <div className='bottom-0 relative md:left-40 pl-8'><Footer /></div>
+                    <footer className="bottom-0 md:fixed relative flex-1 py-6 flex flex-1 flex-col md:pl-64">
+                        <Container.Outer>
+                            <div className="border-t border-zinc-100 pt-4 pb-6 dark:border-zinc-700/40">
+                                <Container.Inner>
+                                    <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+                                        <p className="text-sm text-zinc-400 dark:text-zinc-500">
+                                            &copy; {new Date().getFullYear()} Josh and Emily Calder. All rights
+                                            reserved.
+                                        </p>
+                                        <p className="text-sm text-zinc-400 dark:text-zinc-500">Website by <Link href='https://www.opensaas.au'>OpenSaas</Link></p>
+                                    </div>
+                                </Container.Inner>
+                            </div>
+                        </Container.Outer>
+                    </footer>
                 </AuthContext>
             </body>
         </html>
