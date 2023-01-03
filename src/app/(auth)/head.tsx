f4e56f8651071b431ecc3ada9f76a93025f8bfdb
Josh Calder <josh@opensaas.com.au>
2023-01-03 18:42:44 +1100

    update logo

--- src/app/(auth)/head.tsx
+++ src/app/(auth)/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Sign In - Emily Calder - School of Performing Arts</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
 
-      <link rel="icon" href="/emily-logo.png" />
+      <link rel="icon" href="/favicon.ico" />
     </>
   )
 }
--- src/app/(website)/head.tsx
+++ src/app/(website)/head.tsx
@@ -6,7 +6,7 @@ export default function Head() {
         name="description"
         content="Emily Calder - School of Performing Arts - Teaching Theatre and Music in the Bendigo Region"
       />
-      <link rel="icon" href="/emily-logo.png" />
+      <link rel="icon" href="/favicon.ico" />
     </>
   )
 }
