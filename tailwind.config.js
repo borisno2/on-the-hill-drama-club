abb638f27f8d05d17322286e25fcb669ebe940bc
Josh Calder <josh@opensaas.com.au>
2023-01-06 20:59:17 +1100

    make mobile work on iPhone

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     screens: {
       xs: { max: '575px' }, // Mobile (iPhone 3 - iPhone XS Max).
       sm: { min: '576px' }, // Mobile (matches max: iPhone 11 Pro Max landscape @ 896px).
-      md: { min: '898px' }, // Tablet (matches max: iPad Pro @ 1112px).
+      md: { min: '986px' }, // Tablet (matches max: iPad Pro @ 1112px).
       lg: { min: '1200px' }, // Desktop smallest.
       xl: { min: '1159px' }, // Desktop wide.
       xxl: { min: '1359px' }, // Desktop widescreen.
