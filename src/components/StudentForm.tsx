b9287725b261ec8db637ea890bcd021faa75d93b
Josh Calder <josh.calder@thinkmill.com.au>
2023-01-31 13:30:02 +1100

    update text

--- src/components/StudentForm.tsx
+++ src/components/StudentForm.tsx
@@ -355,10 +355,8 @@ export default function Student({
                   disabled={isSubmitting || !isValid || isDone}
                   className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                  {isSubmitting
+                  {isSubmitting || isDone
                     ? 'Loading...'
-                    : isDone
-                    ? 'Done'
                     : !student
                     ? 'Add'
                     : 'Save'}
