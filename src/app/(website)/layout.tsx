c51d5032952201834168cc3c377b65028dcbaa91
Josh Calder <josh.calder@thinkmill.com.au>
2023-02-08 08:24:28 +1100

    add some loading and error

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -17,13 +17,13 @@ export default function RootLayout({ children }: AppProps) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <AuthContext>
-          <div className="fixed inset-0 flex justify-center sm:px-8">
-            <div className="flex w-full max-w-7xl lg:px-8">
-              <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
-            </div>
+        <div className="fixed inset-0 flex justify-center sm:px-8">
+          <div className="flex w-full max-w-7xl lg:px-8">
+            <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
           </div>
+        </div>
 
+        <AuthContext>
           <div className="relative">
             <Header />
             <main>{children}</main>
--- src/app/dashboard/error.tsx
+++ src/app/dashboard/error.tsx
@@ -1,30 +1,64 @@
-'use client'; // Error components must be Client components
+'use client' // Error components must be Client components
 
-import { useEffect } from 'react';
+import { useEffect } from 'react'
+import { Button } from 'components/Button'
+import Link from 'next/link'
 
 export default function Error({
-    error,
-    reset,
+  error,
+  reset,
 }: {
-    error: Error;
-    reset: () => void;
+  error: Error
+  reset: () => void
 }) {
-    useEffect(() => {
-        // Log the error to an error reporting service
-        console.error(error);
-    }, [error]);
+  useEffect(() => {
+    // Ideally Log the error to an error reporting service
+    console.error(error)
+  }, [error])
 
-    return (
-        <div>
-            <h2>Something went wrong!</h2>
-            <button
+  return (
+    <div className="min-h-full bg-white py-16 px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+      <div className="mx-auto max-w-max">
+        <main className="sm:flex">
+          <p className="text-4xl font-bold tracking-tight text-indigo-600 sm:text-5xl">
+            Oops
+          </p>
+          <div className="sm:ml-6">
+            <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                Something went wrong!
+              </h1>
+              <p className="mt-1 text-base text-gray-500">
+                Please check the URL in the address bar or try refreshing the
+                page
+              </p>
+            </div>
+            <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+              <Link
+                href="/"
+                className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Go back home
+              </Link>
+              <Link
+                href="/contact"
+                className="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Contact
+              </Link>
+              <Button
                 onClick={
-                    // Attempt to recover by trying to re-render the segment
-                    () => reset()
+                  // Attempt to recover by trying to re-render the segment
+                  () => reset()
                 }
-            >
-                Try again
-            </button>
-        </div>
-    );
+                className="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Try Again
+              </Button>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  )
 }
