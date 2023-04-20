d2027b1860e4add0ab3dc73ec6ec5d7e44af1267
Josh Calder <josh@opensaas.com.au>
2023-04-21 08:36:12 +1000

    update

--- src/inngestFunctions/lessonTerms.ts
+++ src/inngestFunctions/lessonTerms.ts
@@ -88,6 +88,47 @@ export const copyTermFunction = inngest.createFunction(
   }
 )
 
+const GET_THIS_LESSON_TERM = gql`
+  query GET_THIS_LESSON_TERM($id: ID!) {
+    lessonTerm(where: { id: $id }) {
+      id
+      lesson {
+        id
+      }
+      term {
+        id
+        copyFrom {
+          id
+        }
+      }
+      enrolments {
+        id
+      }
+    }
+  }
+` as import('../../__generated__/ts-gql/GET_THIS_LESSON_TERM').type
+
+const GET_COPY_FROM_LESSON_TERM = gql`
+  query GET_COPY_FROM_LESSON_TERM($termId: ID!, $lessonId: ID!) {
+    lessonTerms(
+      where: {
+        AND: [
+          { term: { id: { equals: $termId } } }
+          { lesson: { id: { equals: $lessonId } } }
+        ]
+      }
+    ) {
+      id
+      enrolments {
+        id
+        student {
+          id
+        }
+      }
+    }
+  }
+` as import('../../__generated__/ts-gql/GET_COPY_FROM_LESSON_TERM').type
+
 export const copyEnrolmentsFunction = inngest.createFunction(
   'Copy enrolments from one term to another',
   'app/lessonTerm.confirmed',
@@ -96,9 +137,71 @@ export const copyEnrolmentsFunction = inngest.createFunction(
       const { item, session } = event.data
       const context = keystoneContext.withSession(session)
       // get the term with copyFrom Term and lesson for this lessonTerm
+      const newLessonTerm = await context.graphql.run({
+        query: GET_THIS_LESSON_TERM,
+        variables: { id: item.id },
+      })
+      if (!newLessonTerm || !newLessonTerm.lessonTerm)
+        throw new Error('Could not find lessonTerm', { cause: item })
+      if (
+        !newLessonTerm.lessonTerm.term?.copyFrom ||
+        !newLessonTerm.lessonTerm.lesson
+      )
+        throw new Error('No copyFrom term found', { cause: newLessonTerm })
+      if (
+        newLessonTerm.lessonTerm.enrolments &&
+        newLessonTerm.lessonTerm.enrolments.length > 0
+      )
+        throw new Error('Enrolments already exist', { cause: newLessonTerm })
       // get the lessonTerm for the copyFrom Term and lesson
-      // get the enrolments for the lessonTerm
-      // create the enrolments for this lessonTerm
+      const previousLessonTerm = await context.graphql.run({
+        query: GET_COPY_FROM_LESSON_TERM,
+        variables: {
+          termId: newLessonTerm.lessonTerm.term.copyFrom.id,
+          lessonId: newLessonTerm.lessonTerm.lesson.id,
+        },
+      })
+      if (!previousLessonTerm || !previousLessonTerm.lessonTerms)
+        throw new Error('Could not find previous lessonTerm', {
+          cause: newLessonTerm,
+        })
+      if (previousLessonTerm.lessonTerms.length === 0)
+        throw new Error('No previous lessonTerms found', {
+          cause: previousLessonTerm,
+        })
+      if (previousLessonTerm.lessonTerms.length > 1)
+        throw new Error('Too many previous lessonTerms found', {
+          cause: previousLessonTerm,
+        })
+      if (!previousLessonTerm.lessonTerms[0].enrolments)
+        throw new Error('No previous enrolments found', {
+          cause: previousLessonTerm,
+        })
+      if (previousLessonTerm.lessonTerms[0].enrolments.length === 0)
+        throw new Error('No previous enrolments found', {
+          cause: previousLessonTerm,
+        })
+      const enrolments = previousLessonTerm.lessonTerms[0].enrolments.map(
+        (enrolment) => ({
+          status: 'ENROLED',
+          student: {
+            connect: {
+              id: enrolment.student?.id,
+            },
+          },
+        })
+      )
+      const updated = await context.query.LessonTerm.updateOne({
+        where: { id: item.id },
+        data: {
+          enrolments: {
+            create: enrolments,
+          },
+        },
+      })
+      if (!updated)
+        throw new Error('Could not update lessonTerm', { cause: item })
+      return updated
     } catch (error) {
       throw new Error('Error copying enrolments', { cause: error })
     }
