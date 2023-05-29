f3deb346397487c93ced8aac111fdaa4f2b3f881
Josh Calder <josh.calder@thinkmill.com.au>
2023-05-29 19:48:04 +1000

    move nextauth to route handler

--- src/keystone/context/nextAuthFix.ts
+++ src/keystone/context/nextAuthFix.ts
@@ -1,24 +1,22 @@
-
-import { authOptions } from 'pages/api/auth/[...nextauth]'
+import { authOptions } from 'app/api/auth/[...nextauth]/route'
 import { getServerSession } from 'next-auth/next'
-import { cookies, headers } from "next/headers";
-import { keystoneContext } from '.';
-import type { Context } from '.keystone/types';
+import { cookies, headers } from 'next/headers'
+import { keystoneContext } from '.'
+import type { Context } from '.keystone/types'
 
-export async function getServerActionContext(){
-    const req = {
-        headers: Object.fromEntries(headers() as Headers),
-        cookies: Object.fromEntries(
-            cookies()
-                .getAll()
-                .map((c) => [c.name, c.value])
-        ),
-    }
-    const res = { getHeader() { }, setCookie() { }, setHeader() { } }
+export async function getServerActionContext() {
+  const req = {
+    headers: Object.fromEntries(headers() as Headers),
+    cookies: Object.fromEntries(
+      cookies()
+        .getAll()
+        .map((c) => [c.name, c.value])
+    ),
+  }
+  const res = { getHeader() {}, setCookie() {}, setHeader() {} }
 
+  // @ts-expect-error - Forcing getServerSession to work with Server Actions
+  const session = await getServerSession(req, res, authOptions)
 
-    // @ts-expect-error - Forcing getServerSession to work with Server Actions
-    const session = await getServerSession(req, res, authOptions)
-
-    return keystoneContext.withSession(session) as Context
+  return keystoneContext.withSession(session) as Context
 }
