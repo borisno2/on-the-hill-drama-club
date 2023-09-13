6f1bcaa05c771b136731aabde7e6d25248581bf1
Josh Calder <8251494+borisno2@users.noreply.github.com>
2023-09-13 19:55:14 +1000

    format and remove extra exit

--- build-admin-ui.ts
+++ build-admin-ui.ts
@@ -1,14 +1,13 @@
-import {cli} from '@keystone-6/core/scripts/cli'
+import { cli } from '@keystone-6/core/scripts/cli'
 
 // This is a workaround for keystone's next build not exiting
 // we want to just build the admin ui and exit the process
 // we then use next build cli directly in package.json scripts for the next build
-async function main () {
+async function main() {
     try {
         await cli(__dirname, ['build'])
     } catch (error) {
         // swallow error
-        process.exit(0)
     } finally {
         process.exit(0)
     }
