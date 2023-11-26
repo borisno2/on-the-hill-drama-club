aaf8fa7cd9677c5573b579eee74c8a158d315e14
Josh Calder <8251494+borisno2@users.noreply.github.com>
2023-11-26 11:01:01 +1100

    fix login page footer

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -4,7 +4,6 @@ export const baseMetadata: Metadata = {
   title: 'Emily Calder - School of Performing Arts',
   description:
     'Emily Calder - School of Performing Arts - Teaching Theatre and Music in the Bendigo Region',
-  viewport: 'width=device-width, initial-scale=1.0',
 }
 
 export function getMetadata(page: string): Metadata {
