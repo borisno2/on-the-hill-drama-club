4be148b1dc65d278b05d6e2e232813b7c15d5054
Josh Calder <8251494+borisno2@users.noreply.github.com>
2024-04-19 19:19:28 +1000

    remove apple login

--- src/components/SocialLogins.tsx
+++ src/components/SocialLogins.tsx
@@ -12,50 +12,50 @@ export function SocialLogins({ callbackUrl }: { callbackUrl: string }) {
           <span className="bg-white px-2 text-gray-500">Or continue with</span>
         </div>
       </div>
-      <div className="mt-6 grid grid-cols-2 gap-5">
-        <div>
-          <button
-            onClick={() => signIn('google', { callbackUrl })}
-            className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+      {/* <div className="mt-6 grid grid-cols-2 gap-5"> */}
+      <div>
+        <button
+          onClick={() => signIn('google', { callbackUrl })}
+          className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+        >
+          <span className="sr-only">Sign in with Google</span>
+          <svg
+            className="h-5 w-5"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 25 25"
           >
-            <span className="sr-only">Sign in with Google</span>
-            <svg
-              className="h-5 w-5"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 25 25"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-              />
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-              />
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-              />
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-              />
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M1 1h22v22H1z"
-                fill="none"
-              />
-            </svg>
-          </button>
-        </div>
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M1 1h22v22H1z"
+              fill="none"
+            />
+          </svg>
+        </button>
+      </div>
 
-        <div>
+      {/* <div>
           <button
             onClick={() => signIn('apple', { callbackUrl })}
             className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
@@ -74,8 +74,8 @@ export function SocialLogins({ callbackUrl }: { callbackUrl: string }) {
               />
             </svg>
           </button>
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
     </div>
   )
 }
